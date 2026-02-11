34716b35c969e7b417d913ec2e3cfeecd42dccf2
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-02-11 15:36:42 -0800

    Initial build

--- README.md
+++
@@ -1,2 +0,0 @@
-# baumwelt-staging
-For Review Only
---
+++ assets/js/light-dark.js
@@ -0,0 +1,31 @@
+// Requires GDPR compliance
+
+const html = document.querySelector("html");
+const mode = localStorage.getItem("mode");
+
+if (mode === null) switchAuto();
+if (mode === "light") switchLight();
+if (mode === "dark") switchDark();
+
+// three functions used by buttons and radios
+
+function switchAuto() {
+  html.style.setProperty("color-scheme", "dark light");
+  document.getElementById("auto").checked = true;
+  localStorage.removeItem("mode");
+  console.log("Operating system preferred color scheme active");
+}
+
+function switchLight() {
+  html.style.setProperty("color-scheme", "light");
+  document.getElementById("light").checked = true;
+  localStorage.setItem("mode", "light");
+  console.log("Light mode active");
+}
+
+function switchDark() {
+  html.style.setProperty("color-scheme", "dark");
+  document.getElementById("dark").checked = true;
+  localStorage.setItem("mode", "dark");
+  console.log("Dark mode active");
+}
